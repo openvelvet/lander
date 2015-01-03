704d6deef922e12b86d08eec95f2b96ae06ea250
Omar Amiri <omar@openvelvet.com>
2015-01-02 19:16:28 -0800

    some css stuff

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,7 @@
 class Email < ActiveRecord::Base
+
+	validates :email, :first_name, :last_name, :organization, :phone_number, presence: true
+	validates :email, uniqueness: true
+	validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 end
